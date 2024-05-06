bd2b02e7629ba4729969a6cf164e137e41a8afec
Kaktus1549 <hummelotup@seznam.cz>
2024-05-06 21:47:03 +0200

    Update Whoami component with revised bio and fix typo in package.json

--- Frontend/app/_whoami/whoami.tsx
+++ Frontend/app/_whoami/whoami.tsx
@@ -2,7 +2,7 @@ export default function Whoami(){
     return(
     <div id="who">
         <h2 className="left">Who am I?</h2>
-        <p>Hey there! I'm Ondřej, also known as Kaktus1549. High school student diving into cybersecurity, with a passion for computers, history, and airplanes. When I'm not studying, you'll find me gaming, listening to music, or doing sports like airsoft and climbing.</p>
+        <p>Hey there! I&apos;m Ondřej, also known as Kaktus1549. High school student diving into cybersecurity, with a passion for computers, history, and airplanes. When I&apos;m not studying, you&apos;ll find me gaming, listening to music, or doing sports like climbing and airsoft.</p>
     </div>
     );
 }
